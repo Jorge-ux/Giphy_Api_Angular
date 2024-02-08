089d81c835744abfd95157e2a2a6a33480258a45
Jorge-ux <junior.ramirez1306@gmail.com>
2024-02-08 11:55:25 -0500

    Estilos a toda la aplicacion, TERMINADA

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -3,7 +3,7 @@ export const environment = {
 
   apiKey: 'api_key=d6wTZ697lYVlinMvmKB9U6FibItkx3x1',
 
-  complement: '&limit=9&offset=0&rating=g&lang=en&bundle=messaging_non_clips',
+  complement: '&limit=15&offset=0&rating=g&lang=en&bundle=messaging_non_clips',
 
   baseURLFull:
     'https://api.giphy.com/v1/gifs/search?api_key=d6wTZ697lYVlinMvmKB9U6FibItkx3x1&q=homero&limit=9&offset=0&rating=g&lang=en&bundle=messaging_non_clips',
